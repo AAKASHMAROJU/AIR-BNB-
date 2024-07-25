b5e28fd527fe0d5eb7c8e4f9c9fb75108b673a16
aakashmaroju <marojuaakash2003@gmail.com>
2024-07-25 23:45:56 +0530

    added the reviewSchema

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const listingSchema = new Schema({
   title: {
@@ -24,6 +25,12 @@ const listingSchema = new Schema({
   country: {
     type: String,
   },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
---
+++ models/review.js
@@ -0,0 +1,33 @@
+// review => comment, rating, createdAt
+
+const mongoose = require("mongoose");
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/reviews-db");
+}
+
+main()
+  .then(() => console.log("connected Successfully"))
+  .catch((err) => console.log(err));
+
+const { Schema } = mongoose;
+
+const reviewSchema = new Schema({
+  comment: String,
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Review = mongoose.model("Review", reviewSchema); // this review should be associated with the Listings
+
+// each Listing => each Building has number of reviews ...
+// 1 to many relationships
+
+module.exports = Review;
